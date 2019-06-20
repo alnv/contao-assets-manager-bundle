554eac49a9f6203776ac93074caebefff333be26
Alexander Naumov <me@alexandernaumov.de>
2019-06-20 11:57:18 +0200

    add vue

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,3 +1,5 @@
 <?php
 
-//
+$objAssetsManager = \Alnv\ContaoAssetsManagerBundle\Library\AssetsManager::getInstance();
+$objAssetsManager->addIfNotExist( 'bundles/alnvcontaoassetsmanager/js/libraries/vue/vue.min.js' );
+$objAssetsManager->addIfNotExist( 'bundles/alnvcontaoassetsmanager/js/libraries/vue/vue-resource.min.js' );
