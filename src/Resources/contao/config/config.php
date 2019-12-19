dd39c593b84cc96c8e5ee96b28fb298eb439649e
Alexander Naumov <me@alexandernaumov.de>
2019-12-19 10:26:00 +0100

    fix fe token

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,13 +1,15 @@
 <?php
 
+$GLOBALS['ASSETS_MANAGER'] = [];
+$GLOBALS['ASSETS_MANAGER']['BACKEND_PLACEHOLDER'] = '##ALNVCONTAOASSETSMANAGER##';
+
 $objThemeManager = \Alnv\ContaoAssetsManagerBundle\Library\AssetsManager::getInstance();
-if ( TL_MODE == 'BE' ) {
-    $objThemeManager->addIfNotExist( 'bundles/alnvcontaoassetsmanager/js/libraries/core.js' );
-}
 $objThemeManager->addIfNotExist( 'bundles/alnvcontaoassetsmanager/js/libraries/vue/vue.min.js' );
 $objThemeManager->addIfNotExist( 'bundles/alnvcontaoassetsmanager/js/libraries/vue/vue-resource.min.js' );
 
-$GLOBALS['ASSETS_MANAGER'] = [];
-$GLOBALS['ASSETS_MANAGER']['BACKEND_PLACEHOLDER'] = '##ALNVCONTAOASSETSMANAGER##';
-$GLOBALS['TL_MOOTOOLS']['alnvcontaoassetsmanager'] = $GLOBALS['ASSETS_MANAGER']['BACKEND_PLACEHOLDER'];
-$GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = [ 'Alnv\ContaoAssetsManagerBundle\Hooks\BackendTemplate', 'parse' ];
+if ( TL_MODE == 'BE' ) {
+
+    $GLOBALS['TL_MOOTOOLS']['alnvcontaoassetsmanager'] = $GLOBALS['ASSETS_MANAGER']['BACKEND_PLACEHOLDER'];
+    $GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = [ 'Alnv\ContaoAssetsManagerBundle\Hooks\BackendTemplate', 'parse' ];
+    $objThemeManager->addIfNotExist( 'bundles/alnvcontaoassetsmanager/js/libraries/core.js' );
+}
