15602d057bd674136caeea41972258304c09831b
Alexander Naumov <me@alexandernaumov.de>
2020-01-28 10:42:11 +0100

    impl alert component

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -6,6 +6,7 @@ $GLOBALS['ASSETS_MANAGER']['BACKEND_PLACEHOLDER'] = '##ALNVCONTAOASSETSMANAGER##
 $objThemeManager = \Alnv\ContaoAssetsManagerBundle\Library\AssetsManager::getInstance();
 $objThemeManager->addIfNotExist( 'bundles/alnvcontaoassetsmanager/js/libraries/vue/vue.min.js' );
 $objThemeManager->addIfNotExist( 'bundles/alnvcontaoassetsmanager/js/libraries/vue/vue-resource.min.js' );
+$objThemeManager->addIfNotExist( 'bundles/alnvcontaoassetsmanager/js/vue/components/alert-component.js' );
 $objThemeManager->addIfNotExist( 'bundles/alnvcontaoassetsmanager/js/vue/components/loading-component.js' );
 
 if ( TL_MODE == 'BE' ) {
---
+++ src/Resources/public/js/vue/components/alert-component.js
@@ -0,0 +1,53 @@
+const AlertComponent = Vue.component( 'alert', {
+    data: function () {
+        return {
+            timeoutId: null,
+            alert: this.$parent.alert
+        }
+    },
+    methods: {
+        getCssClasses: function () {
+            var objClass = {};
+            objClass['active'] = this.alert.active;
+            if ( this.alert.type ) {
+                objClass[ this.alert.type ] = this.alert.type
+            }
+            if ( this.alert.component ) {
+                objClass[ this.alert.component ] = this.alert.component
+            }
+            return objClass;
+        },
+        clearAlert: function () {
+            if ( this.alert.type === 'error' ) {
+                this.$parent.clearAlert();
+                if (this.timeoutId) {
+                    clearTimeout(this.timeoutId);
+                }
+            }
+        }
+    },
+    watch: {
+        alert: {
+            handler: function (alert) {
+                if ( alert.type === 'error' && !this.timeoutId ) {
+                    this.timeoutId = setTimeout(function () {
+                        this.$parent.clearAlert();
+                        clearTimeout(this.timeoutId);
+                    }.bind(this),1000)
+                }
+            },
+            deep: true
+        }
+    },
+    mounted: function () {},
+    props: {},
+    template:
+        '<div v-if="alert.active" class="alert-component" v-bind:class="this.getCssClasses()" @click="clearAlert()">' +
+            '<div class="alert-container">' +
+                '<slot>' +
+                    '<loading></loading>' +
+                    '<p v-if="alert.message">{{ alert.message }}</p>' +
+                '</slot>' +
+            '</div>' +
+        '</div>'
+});
