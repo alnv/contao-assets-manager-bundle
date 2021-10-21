44474cb7ed63426ff722a3ae7691e666d987707c
Alexander Naumov <mail@alexandernaumov.de>
2021-10-21 13:03:28 +0200

    impl remove assets func

--- src/Library/AssetsManager.php
+++ src/Library/AssetsManager.php
@@ -2,37 +2,40 @@
 
 namespace Alnv\ContaoAssetsManagerBundle\Library;
 
-
 class AssetsManager extends \Combiner {
-
 
     protected static $arrManager = [];
     protected static $objInstance = null;
 
-
     public static function getInstance() {
 
-        if ( null === self::$objInstance ) {
-
+        if (null === self::$objInstance) {
             self::$objInstance = new self;
         }
 
         return self::$objInstance;
     }
 
+    public function addIfNotExist($strFile) {
 
-    public function addIfNotExist( $strFile ) {
+        $arrFiles = explode('/', $strFile);
+        $strFilename = end($arrFiles);
 
-        $strFilename = end ( explode('/', $strFile ) );
-
-        if ( !array_key_exists( $strFilename, self::$arrManager ) ) {
-
-            self::$arrManager[ $strFilename ] = $strFile;
+        if (!array_key_exists($strFilename, self::$arrManager)) {
+            self::$arrManager[$strFilename] = $strFile;
         }
     }
 
+    public function removeFile($strFile) {
 
-    public function getCombinedAssets( $strUrl = null, $strVersion = null, $strMedia = 'screen' ) {
+        $arrFiles = explode('/', $strFile);
+        $strFilename = end($arrFiles);
+        if (array_key_exists($strFilename, self::$arrManager)) {
+            unset(self::$arrManager[$strFilename]);
+        }
+    }
+
+    public function getCombinedAssets($strUrl = null, $strVersion = null, $strMedia = 'screen') {
 
         $this->addMultiple( self::$arrManager, $strVersion, $strMedia );
 
