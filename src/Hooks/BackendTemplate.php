4eae6bdc82404b05ce0692539c8baf6c525021bd
Alexander Naumov <mail@alexandernaumov.de>
2021-12-28 14:25:24 +0100

    add new icons

--- src/Hooks/BackendTemplate.php
+++ src/Hooks/BackendTemplate.php
@@ -2,16 +2,14 @@
 
 namespace Alnv\ContaoAssetsManagerBundle\Hooks;
 
-
 class BackendTemplate {
-
 
     public function parse( $strContent, $strTemplate ) {
 
-        if ( $strTemplate == 'be_main' ) {
+        if ($strTemplate == 'be_main') {
 
             $objAssets = new \FrontendTemplate('js_assetsmanager');
-            $strContent = str_replace( $GLOBALS['ASSETS_MANAGER']['BACKEND_PLACEHOLDER'] , $objAssets->parse(), $strContent );
+            $strContent = str_replace($GLOBALS['ASSETS_MANAGER']['BACKEND_PLACEHOLDER'] , $objAssets->parse(), $strContent);
         }
 
         return $strContent;
