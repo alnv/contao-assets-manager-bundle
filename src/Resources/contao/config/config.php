d6ac7652a4853f16e025a43fe64a78812b2b540c
Alexander Naumov <alexandernaumov-privat@MBPvonAlexander.fritz.box>
2022-03-25 18:12:11 +0100

    fix assets

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,13 +3,15 @@
 $GLOBALS['ASSETS_MANAGER'] = [];
 $GLOBALS['ASSETS_MANAGER']['BACKEND_PLACEHOLDER'] = '##ALNVCONTAOASSETSMANAGER##';
 
-$objThemeManager = \Alnv\ContaoAssetsManagerBundle\Library\AssetsManager::getInstance();
-$objThemeManager->addIfNotExist('bundles/alnvcontaoassetsmanager/js/libraries/vue/vue.min.js');
-$objThemeManager->addIfNotExist('bundles/alnvcontaoassetsmanager/js/libraries/vue/vue-resource.min.js');
-$objThemeManager->addIfNotExist('bundles/alnvcontaoassetsmanager/js/vue/components/alert-component.js');
-$objThemeManager->addIfNotExist('bundles/alnvcontaoassetsmanager/js/vue/components/loading-component.js');
+if (class_exists('Alnv\ContaoAssetsManagerBundle\Library\AssetsManager')) {
+    $objThemeManager = \Alnv\ContaoAssetsManagerBundle\Library\AssetsManager::getInstance();
+    $objThemeManager->addIfNotExist('bundles/alnvcontaoassetsmanager/js/libraries/vue/vue.min.js');
+    $objThemeManager->addIfNotExist('bundles/alnvcontaoassetsmanager/js/libraries/vue/vue-resource.min.js');
+    $objThemeManager->addIfNotExist('bundles/alnvcontaoassetsmanager/js/vue/components/alert-component.js');
+    $objThemeManager->addIfNotExist('bundles/alnvcontaoassetsmanager/js/vue/components/loading-component.js');
+}
 
-if ( TL_MODE == 'BE' ) {
+if (TL_MODE == 'BE') {
     $GLOBALS['TL_CSS']['alnvcontaoassetsmanager'] = 'bundles/alnvcontaoassetsmanager/css/contao.css';
     $GLOBALS['TL_MOOTOOLS']['alnvcontaoassetsmanager'] = $GLOBALS['ASSETS_MANAGER']['BACKEND_PLACEHOLDER'];
     $GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = ['Alnv\ContaoAssetsManagerBundle\Hooks\BackendTemplate', 'parse'];
